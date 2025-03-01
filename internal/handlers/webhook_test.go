4be27809070a03326927cb5e66784a000a957e6f
RahatMelsov <rahatmelsovforjob@gmai.com>
2025-03-01 18:06:36 +0500

    fix

--- internal/handlers/webhook_test.go
+++ internal/handlers/webhook_test.go
@@ -21,6 +21,14 @@ func TestMain(m *testing.M) {
 	// Parse flags for testing
 	config.ServerParseFlag()
 
+	filePath := storage.ServerFlags.FileStoragePath // Change this to the file you want to delete
+
+	err := os.Remove(filePath)
+	if err != nil {
+		fmt.Println("Error deleting file:", err)
+		//return
+	}
+
 	// Run tests
 	code := m.Run()
 	os.Exit(code)
