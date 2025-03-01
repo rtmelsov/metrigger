7bd938ea0177c6f2264dc89b8f21368c33baa7b7
RahatMelsov <rahatmelsovforjob@gmai.com>
2025-03-01 17:41:05 +0500

    feat: Добавили шифровку

--- internal/middleware/jwt.go
+++ internal/middleware/jwt.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"github.com/rtmelsov/metrigger/internal/helpers"
 	"github.com/rtmelsov/metrigger/internal/storage"
+	"go.uber.org/zap"
 	"io"
 	"net/http"
 )
 
 func JwtParser(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		storage.GetMemStorage().GetLogger().Info("jwt parser", zap.String("jwt", storage.ServerFlags.JwtKey))
 		if storage.ServerFlags.JwtKey != "" {
 			body, err := io.ReadAll(r.Body)
 			if err != nil {
@@ -20,6 +22,9 @@ func JwtParser(h http.Handler) http.Handler {
 			expectedHash := helpers.ComputeHMACSHA256(body, storage.ServerFlags.JwtKey)
 			receivedHash := r.Header.Get("HashSHA256")
 
+			storage.GetMemStorage().GetLogger().Info("comparing hashes",
+				zap.String("expectedHash", expectedHash),
+				zap.String("receivedHash", receivedHash))
 			if receivedHash != expectedHash {
 				http.Error(w, "Invalid hash", http.StatusBadRequest)
 				return
