a2f7ce5b894d153c73310929d09341fbe6f98ecd
RahatMelsov <rahatmelsovforjob@gmai.com>
2025-07-02 20:36:12 +0500

    fix: добавлены переменные для отображения данных билда при старте приложения

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/rtmelsov/metrigger/cmd/staticlint"
 	"github.com/rtmelsov/metrigger/internal/agent"
 	"github.com/rtmelsov/metrigger/internal/config"
 	"go.uber.org/zap"
 )
 
+var (
+	buildVersion string = "N/A"
+	buildDate    string = "N/A"
+	buildCommit  string = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\r\n", buildVersion)
+	fmt.Printf("Build date: %s\r\n", buildDate)
+	fmt.Printf("Build commit: %s\r\n", buildCommit)
 	staticlint.Check()
 
 	logger := config.GetAgentConfig().GetLogger()
